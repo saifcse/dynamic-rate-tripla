928694b0849412fc7ab4d43dc4659eb1d655ceb0
Saif Shobuz <skcse.21@gmail.com>
2026-02-28 23:57:34 +0900

    Code clean-up and a small fix for log_trace_id

--- app/services/concerns/service_loggable.rb
+++ app/services/concerns/service_loggable.rb
@@ -1,32 +1,30 @@
-# module Concerns
-  module ServiceLoggable
-    extend ActiveSupport::Concern
+module ServiceLoggable
+  extend ActiveSupport::Concern
 
-    private
+  private
 
-    def log_trace_id
-      @trace_id ||= "#{SecureRandom.uuid}"
-    end
-
-    def log_info(event, extra = {})
-      write_log(:info, event, extra)
-    end
-
-    def log_error(event, extra = {})
-      write_log(:error, event, extra)
-    end
-
-    def write_log(level, event, extra)
-      log_data = {
-        timestamp: Time.current,
-        event: event,
-        trace_id: log_trace_id, # Unique identifier for tracing
-        service: self.class.name,
-        hotel_id: @hotel,
-        room_id: @room 
-      }.merge(extra)
-
-      Rails.logger.send(level, log_data.to_json)
-    end
+  def log_trace_id
+    @trace_id ||= SecureRandom.uuid
   end
-# end
+
+  def log_info(event, extra = {})
+    write_log(:info, event, extra)
+  end
+
+  def log_error(event, extra = {})
+    write_log(:error, event, extra)
+  end
+
+  def write_log(level, event, extra)
+    log_data = {
+      timestamp: Time.current,
+      event: event,
+      trace_id: log_trace_id, # Unique identifier for tracing
+      service: self.class.name,
+      hotel_id: @hotel,
+      room_id: @room 
+    }.merge(extra)
+
+    Rails.logger.send(level, log_data.to_json)
+  end
+end
